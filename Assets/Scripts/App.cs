4b84b5f484f7e73064798b0f782bb986db361348
Andrei Marks <hey@ndrei.us>
2020-09-04 00:53:20 -0700

    Managed to just get through some Input stuff.

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,17 @@
+﻿using Bones.TextWriter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+    void Start()
+    {
+        SetUpGame();
+    }
+
+    private void SetUpGame()
+    {
+        var textWriter = new TextWriter();
+    }
+}
---
+++ Assets/Scripts/TextWriter/TextWriter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Bones.TextWriter
+{
+    public class TextWriter
+    {
+        public TextWriter()
+        {
+            Keyboard.current.onTextInput += OnTextInput;
+        }
+
+        private void OnTextInput(char charInput)
+        {
+            Debug.Log(charInput);
+        }
+    }
+}
