6ea8073f0a231f601fc7298702cec117c850e141
Andrei Marks <hey@ndrei.us>
2020-09-07 09:58:38 -0700

    Other random updates.

---
+++ Assets/App.cs
@@ -0,0 +1,17 @@
+﻿using Bones.TextWriter;
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+    void Start()
+    {
+        var twui = new WordProcessorUI();
+        var tw = new TextWriter(twui);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
