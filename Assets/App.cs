50f42ae589c7ccfe7f4d3734bf33c20ad9dca25d
andmarks <hey@ndrei.us>
2020-09-12 13:05:32 -0700

    Checkpoint commit for 20200912-Chess.

---
+++ Assets/App.cs
@@ -0,0 +1,19 @@
+﻿using Bones.Chess;
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create board
+        // Create pieces
+        // Create interactions
+        CreateBoard();
+    }
+
+    private void CreateBoard()
+    {
+        ChessBoard grid = new ChessBoard();
+    }
+}
